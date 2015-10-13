71a7c51ea1716970a262f8e218cfff8fb9014de5
luocheng <1059326760@qq.com>
2015-10-13 09:15:32 +0800

    Initial Commit

---
+++ TabbarSelectDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TabbarSelectDemo
+//
+//  Created by 罗成 on 15/10/13.
+//  Copyright (c) 2015年 罗成. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
